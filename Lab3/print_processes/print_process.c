8828d8eed8f96330d9471f43ba180746011c74be
guo602 <370040709@qq.com>
2023-12-05 17:01:27 +0800

    finish print process info

--- Lab3/print_processes/print_process.c
+++ Lab3/print_processes/print_process.c
@@ -5,23 +5,31 @@
 # include <linux/kernel.h>
 # include <linux/list.h>
 # include <linux/init_task.h> // init_task
+# include <linux/fs_struct.h>
+# include <linux/fdtable.h>
+# include <asm/atomic.h>
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Print PID of all processes");
 MODULE_AUTHOR("Burger");
 
 static int __init print_process_module_init(void){
-    printk("process print module init\n");
 
     struct task_struct *p,*ts;
     struct list_head *pos;
     int p_count=0;
     p = &init_task;
+    
+    printk("process print module init\n");
+
     list_for_each(pos,&p->tasks){
         ts = list_entry(pos,struct task_struct,tasks);
-        printk("%s pid:%d\n",ts->comm,ts->pid);   
+        printk("%s ; pid:%d ; state:%u ; prio:%d ; static_prio:%d ; parent'pid:%d ; file counts:%d ; umask:%d;", \
+                ts->comm,ts->pid,ts->__state,ts->prio,ts->static_prio,(ts->parent)->pid,atomic_read(&(ts->files)->count),(ts->fs)->umask);   
         p_count+=1;
-        // printk("process\n");
+        if(ts->mm != NULL){
+            printk("total vm:%ld;\n",(ts->mm)->total_vm);
+        }
 
     }
     printk("Total process-count:%d\n",p_count);
